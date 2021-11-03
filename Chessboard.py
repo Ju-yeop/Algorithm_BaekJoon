6a422c85d1fcaf0e3d005f13c9d5a1d013d5da82
Ju-yeop <dhwnduq12@naver.com>
2021-11-03 15:29:44 +0900

    Chess cpt

--- Chessboard.py
+++ Chessboard.py
@@ -5,42 +5,23 @@ for _ in range(h):
     temp_ls = list(input())
     ls.append(temp_ls)
 
+
 for k in range(h - 7):
     for p in range(w - 7):
         wcount = 0
         bcount = 0
         for i in range(8):
             for j in range(8):
-                if True:
-                    if i % 2 == 0:
-                        if j % 2 == 0:
-                            if ls[i + k][j + p] != 'B':
-                                bcount += 1
-                        elif j % 2 == 1:
-                            if ls[i + k][j + p] != 'W':
-                                bcount += 1
-                    elif i % 2 == 1:
-                        if j % 2 == 0:
-                            if ls[i + k][j + p] != 'W':
-                                bcount += 1
-                        elif j % 2 == 1:
-                            if ls[i + k][j + p] != 'B':
-                                bcount += 1
-                if True:
-                    if i % 2 == 0:
-                        if j % 2 == 0:
-                            if ls[i + k][j + p] != 'W':
-                                wcount += 1
-                        elif j % 2 == 1:
-                            if ls[i + k][j + p] != 'B':
-                                wcount += 1
-                    elif i % 2 == 1:
-                        if j % 2 == 0:
-                            if ls[i + k][j + p] != 'B':
-                                wcount += 1
-                        elif j % 2 == 1:
-                            if ls[i + k][j + p] != 'W':
-                                wcount += 1
+                if (i+j) % 2 == 0:
+                    if ls[i + k][j + p] != 'B':
+                        bcount += 1
+                    else:
+                        wcount += 1
+                elif (i+j) % 2 == 1:
+                    if ls[i + k][j + p] != 'W':
+                        bcount += 1
+                    else:
+                        wcount += 1
         count_ls.append(wcount)
         count_ls.append(bcount)
 print(min(count_ls))
