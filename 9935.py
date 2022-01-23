39ba96dc4f782a99b37b5df6c36462b60411ceac
Ju-yeop <dhwnduq12@naver.com>
2022-01-24 01:10:14 +0900

    Stack

---
+++ 9935.py
@@ -0,0 +1,36 @@
+"""
+import sys
+
+S = sys.stdin.readline().strip()
+target = sys.stdin.readline().strip()
+
+while True:
+    if target in S:
+        S = S.replace(target, "")
+    else:
+        break
+
+print("FRULA" if len(S) == 0 else S)
+"""
+
+#Stack 사용
+
+import sys
+
+S = sys.stdin.readline().strip()
+target = sys.stdin.readline().strip()
+
+ls = []
+for i in range(len(S)):
+    ls.append(S[i])
+    if ls[-1] == target[-1]:
+        judge = True
+        for j in range(len(target)):
+            if ls[len(ls)-j-1] != target[len(target)-j-1]:
+                judge = False
+                break
+        if judge == True:
+            for v in range(len(target)):
+                ls.pop()
+
+print(''.join(ls) if len(ls) != 0 else 'FRULA')
