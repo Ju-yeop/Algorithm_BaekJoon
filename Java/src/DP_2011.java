4b660629ee7f0c25e879a742e43c51a50ff763cc
Ju-yeop <dhwnduq12@naver.com>
2023-11-13 05:11:40 +0900

    DP : 골드5 (X)

---
+++ Java/src/DP_2011.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/*
+25114
+ */
+public class DP_2011 {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] dp = new int[str.length()+1];
+        Arrays.fill(dp, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= str.length(); i++){
+            int now = str.charAt(i-1) - '0';
+            if (now >= 1 && now <= 9){
+                dp[i] += dp[i-1];
+                dp[i] %= 1000000;
+            }
+
+            if (i == 1) continue;
+
+            int prev = str.charAt(i-2) - '0';
+            if (prev == 0) continue;
+
+            int value = prev * 10 + now;
+            if (value >= 10 && value <= 26){
+                dp[i] += dp[i-2];
+                dp[i] %= 1000000;
+            }
+        }
+        System.out.println(dp[str.length()]);
+    }
+}
