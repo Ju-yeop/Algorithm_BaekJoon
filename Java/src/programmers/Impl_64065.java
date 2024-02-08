8e6dac084c79d8b0b4e8cfddb60541ff600c0a61
Ju-yeop <dhwnduq12@naver.com>
2024-02-08 10:31:59 +0900

    Impl : LV2 (O)

---
+++ Java/src/programmers/Impl_64065.java
@@ -0,0 +1,25 @@
+package programmers;
+
+import java.util.*;
+
+class Impl_64065 {
+    public ArrayList<Integer> solution(String s) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        String[] str = s.substring(2, s.length() - 2).replace("},{", "-").split("-");
+        Arrays.sort(str, new Comparator<String>() {
+            public int compare(String o1, String o2) {
+
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
+        for (String x : str) {
+            String[] temp = x.split(",");
+            for (String value : temp) {
+                int n = Integer.parseInt(value);
+                if (!answer.contains(n))
+                    answer.add(n);
+            }
+        }
+        return answer;
+    }
+}
