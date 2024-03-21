4feb9fb1f8d9128d44a35ac210a0e36073ea2c9f
Ju-yeop <dhwnduq12@naver.com>
2024-03-21 12:12:33 +0900

    PQ: G4 (O)

--- Java/src/Baekjoon/PQ_7662.java
+++ Java/src/Baekjoon/PQ_7662.java
@@ -3,6 +3,7 @@ package Baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -37,8 +38,8 @@ public class PQ_7662 {
         T = Integer.parseInt(br.readLine());
         for (int testCase = 0; testCase < T; testCase++){
             N = Integer.parseInt(br.readLine());
-            PriorityQueue<Integer> minH = new PriorityQueue<>((i1, i2) -> i1 - i2);
-            PriorityQueue<Integer> maxH = new PriorityQueue<>((i1, i2) -> i2 - i1);
+            PriorityQueue<Integer> minH = new PriorityQueue<>();
+            PriorityQueue<Integer> maxH = new PriorityQueue<>(Collections.reverseOrder());
             Map<Integer, Integer> map = new HashMap<>();
             for (int i = 0; i < N; i++){
                 st = new StringTokenizer(br.readLine());
